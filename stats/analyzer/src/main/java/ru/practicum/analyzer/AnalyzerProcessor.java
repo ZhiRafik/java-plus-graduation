73b6122ce43b9f26e17c0094c6ee0b42fc6d38b8
ZhiRafik <roketro009@gmail.com>
2025-11-11 15:25:37 +0300

    Добавить аннотацию компоненты на анализатор

--- stats/analyzer/src/main/java/ru/practicum/analyzer/AnalyzerProcessor.java
+++ stats/analyzer/src/main/java/ru/practicum/analyzer/AnalyzerProcessor.java
@@ -2,9 +2,11 @@ package ru.practicum.analyzer;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 import ru.practicum.analyzer.processor.EventSimilarityProcessor;
 import ru.practicum.analyzer.processor.UserActionProcessor;
 
+@Component
 @RequiredArgsConstructor
 public class AnalyzerProcessor implements CommandLineRunner {
     private final EventSimilarityProcessor eventSimilarityProcessor;
