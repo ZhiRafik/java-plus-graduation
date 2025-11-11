4ef509298574ad9faff4e069d94a97cfd0f0bd12
ZhiRafik <roketro009@gmail.com>
2025-11-11 14:49:10 +0300

    Добавить аннотацию, почему-то не регистрируется в eureka

--- stats/aggregator/src/main/java/ru/practicum/aggregator/AggregatorApplication.java
+++ stats/aggregator/src/main/java/ru/practicum/aggregator/AggregatorApplication.java
@@ -3,9 +3,11 @@ package ru.practicum.aggregator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import ru.practicum.aggregator.service.AggregatorStarter;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class AggregatorApplication {
